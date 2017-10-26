f257838ba7d525bf3f4d3e67b3aef06e41cf6560
Ahmed Umar <khattabu@gmail.com>
2017-10-26 17:35:14 +0100

    Added Pref class

---
+++ app/src/main/java/com/example/ahmed/alcassessment/data/local/Prefs.java
@@ -0,0 +1,39 @@
+package com.example.ahmed.alcassessment.data.local;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.preference.PreferenceManager;
+
+import com.example.ahmed.alcassessment.R;
+
+import javax.inject.Inject;
+
+/**
+ * Created by ahmed on 10/26/17.
+ */
+
+public class Prefs {
+    private static SharedPreferences preferences;
+    private static Resources resources;
+    @Inject
+    Prefs(Context context){
+        preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        resources = context.getResources();
+    }
+
+    public static boolean createRandomCard(){
+        return preferences.getBoolean(resources.getString
+                (R.string.preload_random_key), false);
+    }
+
+    public static boolean twoWayExchange(){
+        return preferences.getBoolean(resources.getString(R.string.two_way_key),
+                true);
+    }
+
+    public static int numberOfCards(){
+        return Integer.valueOf(preferences.getString(resources.getString
+                (R.string.preload_number_key), "8"));
+    }
+}
